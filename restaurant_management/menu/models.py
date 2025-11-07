33aa1134ca0f3ab1c589b1880aea76db1aa4c49f
Kad335 <yodanandrade13@gmail.com>
2025-11-07 02:27:06 -0300

    Update models.py

--- restaurant_management/menu/models.py
+++ restaurant_management/menu/models.py
@@ -6,6 +6,9 @@ class MenuItem(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     available = models.BooleanField(default=True)
 
+    class Meta:
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
