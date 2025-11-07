262ae70611add503d6d35b0aaa8029ae3fab0994
bbnadev <nicolas.bahamondeb@gmail.com>
2025-11-07 01:50:25 -0300

    Actualizar la configuración de la base de datos a MySQL en settings.py

--- restaurant_management/restaurant_management/settings.py
+++ restaurant_management/restaurant_management/settings.py
@@ -79,8 +79,12 @@ WSGI_APPLICATION = 'restaurant_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rms_db',
+        'USER': 'root',
+        'PASSWORD': 'Inacap.2025',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
