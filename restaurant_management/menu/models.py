9ec298a0b7b7a6c52377869c19ae30acc7a9b029
Kad335 <yodanandrade13@gmail.com>
2025-11-07 01:24:26 -0300

    Add MenuItem model for restaurant menu

--- restaurant_management/menu/models.py
+++ restaurant_management/menu/models.py
@@ -1,3 +1,14 @@
+
 from django.db import models
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
 
 # Create your models here.
